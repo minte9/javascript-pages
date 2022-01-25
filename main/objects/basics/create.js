c89a5abcaef7c9bb321243a867037b4d575ca1cb
minte9 <catalinprescure@gmail.com>
2022-01-25 13:43:21 +0200

    javascript-pages update

---
+++ main/objects/basics/create.js
@@ -0,0 +1,36 @@
+/**
+ * Object are created using figure brakets {}
+ * 
+ * A property is a (key:value) pair
+ */
+
+
+/**
+ * Empty object, with no properties
+ */
+let A = {};
+console.log(A.something); // undefined
+
+
+/**
+ * Object with properties
+ */
+let B = {
+    name: "John",
+    age: 30,  // trailing comma - accepted
+};
+console.log(B.name); // John
+console.log(B.age); // 30
+delete B.age; // remove a property
+console.log(B.age); // undefined
+
+
+/**
+ * Computed properties
+ */
+let myvar = "my key";
+let C = {
+    "some key": 1,
+    [myvar]: 3,
+}
+console.log(C[myvar]); // 3
