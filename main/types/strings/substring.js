385390686b6ee6f05f08fd1f914587d2405b387c
minte9 <catalinprescure@gmail.com>
2022-01-26 19:13:49 +0200

    javascript-pages update

--- main/types/strings/substring.js
+++ main/types/strings/substring.js
@@ -2,13 +2,17 @@
  * Substring methods
  */
 
-console.log("Widget with id".includes('id')); 
-    // true
-console.log('Widget'.includes('id', 2)); 
-    // false
-console.log('Widget'.startsWith('Wid')); 
-    // true
-console.log('Widget'.endsWith('get')); 
-    // true
-console.log('Widget'.slice(0, 3)); 
-    // Wid
+// Includes
+console.log("Widget with id".includes('id')); // true
+console.log('Widget'.includes('id', 2)); // false
+
+// Start with
+console.log('Widget'.startsWith('Wid')); // true
+console.log('Widget'.endsWith('get')); // true
+
+// Substring
+console.log('Widget'.slice(0, 3)); // Wid
+
+// Position
+console.log('Widget'.charAt('0')); // W
+console.log('Widget'.indexOf('id')); // 1
