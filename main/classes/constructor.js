ea23e6e20bb6322f28f93582d1fbc9d83fb0e994
minte9 <catalinprescure@gmail.com>
2022-01-27 15:27:22 +0200

    javascript-pages update

---
+++ main/classes/constructor.js
@@ -0,0 +1,30 @@
+/**
+ * Classes
+ * 
+ * We already know about new function() contructor.
+ * 
+ * Classes are an advanced object construct.
+ * It introduce great new features useful for OOP.
+ * 
+ * In JavaScript, a class is a kind of function.
+ * Within the class, no commas are required. 
+ * 
+ * Classe always use strict.
+ */
+
+class User
+{
+    constructor (name) {
+        this.name = name;
+    }
+
+    hello() {
+        console.log("Hi, this is " + this.name);
+    }
+}
+ 
+let a = new User("Mihai");
+let b = new User("John");
+
+a.hello(); // Hi, this is Mihai
+b.hello(); // Hi, this is John
---
+++ main/classes/getter.js
@@ -0,0 +1,26 @@
+/**
+ * Get / Set
+ */
+
+class Message
+{
+    constructor (msg) {
+        this.msg = msg; // invokes the setter
+    }
+
+    set msg(str) {
+        str = str.toUpperCase();
+        this._msg = str; // Look Here
+    }
+
+    get msg() {
+        return this._msg;
+    }
+}
+ 
+let obj = new Message("hello world");
+console.log(obj.msg + "!"); // HELLO WORLD!
+
+try {
+    console.log(obj.msg()); // Error: msg is not a function
+} catch (err) {}
