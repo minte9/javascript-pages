c0ae8e1bbea51210c9d1faab540254436d614ab4
minte9 <catalinprescure@gmail.com>
2022-01-27 16:20:57 +0200

    javascript-pages update

--- main/classes/binding/bind.js
+++ main/classes/binding/bind.js
@@ -1,13 +1,22 @@
 /**
+ * Method binding:
+ * 
  * There are two ways of fixing 'losing this' problem:
  * 
  * Pass a wrapper-function or ...
  * Bind the method to object in constructor.
+ * 
+ * The bind() method takes an object as an first argument 
+ * and creates a new function.
+ */
+
+/**
+ * First example
  */
 class Button
 {
     constructor() {
-        this.click = this.click.bind(this); // bind in constructor
+        this.click = this.click.bind(this); // method bind
     }
 
     click() {
@@ -24,9 +33,26 @@ class Button
 }
 
 let button = new Button();
-
 setTimeout(button.click, 1000); // Click
 setTimeout(button.blur, 1000); // Blur
 setTimeout(
     () => button.focus(), 1000 // Focus
 );
+
+
+/**
+ * Second example
+ */
+class User {
+    getName() {
+        return this.name;
+    }
+}
+
+let obj = new User();
+let user1 = {name: "John"};
+let user2 = {name: "Ana"};
+
+console.log(obj.getName()); // undefiend
+console.log(obj.getName.bind(user1)()); // John
+console.log(obj.getName.bind(user2)()); // Ana
