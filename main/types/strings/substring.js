1de50ad98e6624818828c8eeeca39245f0638a79
minte9 <catalinprescure@gmail.com>
2022-01-27 09:40:38 +0200

    javascript-pages update

--- main/types/strings/substring.js
+++ main/types/strings/substring.js
@@ -16,3 +16,9 @@ console.log('Widget'.slice(0, 3)); // Wid
 // Position
 console.log('Widget'.charAt('0')); // W
 console.log('Widget'.indexOf('id')); // 1
+
+// Search
+console.log('Widget with id'.indexOf('Widget')); // 0
+console.log('Widget with id'.indexOf('widget')); // -1
+console.log('Widget with id'.indexOf("id")); // 1
+console.log('Widget with id'.indexOf('id', 2)); // 12
