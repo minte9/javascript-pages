7aef23f498275961a17d4781af6a943908002231
minte9 <catalinprescure@gmail.com>
2022-01-27 12:40:26 +0200

    javascript-pages update

--- main/types/arrays/methods.js
+++ main/types/arrays/methods.js
@@ -8,12 +8,10 @@ let A = [
     {id: 3, user: 'Marry'},
 ];
 let B = A.filter(item => item.id < 2);
-
 console.log(B); // {id: 1, user: 'John'}
 
 let C = ["Bilbo", "Gandalf"];
 let D = C.map(item => item.length);
-
 console.log(D); // [5, 7]
 
 let E = [
@@ -22,5 +20,4 @@ let E = [
     {id: 3, user: 'Marry'},
 ];
 E.sort((a, b) => a.id - b.id);
-
 console.log(E[0]); // {id: 2, user: 'Pete'},
