a00e8a8c86ac40d0aa54ae5658cd48bbd3beb152
minte9 <catalinprescure@gmail.com>
2022-11-02 14:39:59 +0200

    javascript-pages update

--- main/fundamentals/code_structure/statements.js
+++ main/fundamentals/code_structure/statements.js
@@ -1,6 +1,8 @@
 /**
  * Between statements the semicolon can be omitted, but ...
  * it is recommended to put semicolons every time.
+ * 
+ * For example:
  * Javascript does not assume a semicolon before square brackets.
  */
 console.log("Hello");
@@ -12,3 +14,7 @@ console.log(1 +
     2 + 
     3
 ); // 6
+
+// console.log("No semicolon")
+// [1, 2].forEach(console.log); 
+    // Uncaught TypeError
