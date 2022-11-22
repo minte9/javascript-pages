6f0d4c46467dfaab638d6e7c9b09009e3eb13905
minte9 <catalinprescure@gmail.com>
2022-11-22 12:56:01 +0200

    javascript-pages update

--- main/fundamentals/maths/exponentiation.js
+++ main/fundamentals/maths/exponentiation.js
@@ -1,11 +1,12 @@
 /**
- * The exponentiation operator a ** b raises a to the power of b.
- * A in maths, square root is an exponentiation by 1/2 
+ * The exponentiation operator a ** b raises a to the power of b
+ * Power of 1/2 is the same as square root
+ * Power of 1/3 is the same as cubic root
  */
 
-console.log(2 ** 3); // 8
-console.log(4 ** 1/2); // 2 (power of 1/2 is the same as square root)
-console.log(8 ** 1/3); // 2 (power of 1/3 is the same as cubic root)
+console.log(2 ** 3);    // 8
+console.log(4 ** 1/2);  // 2
+console.log(8 ** 1/3);  // 2
 
 console.log(2 ^ 3); // 1 - XOR bitwise
 console.log(2 ^ 2); // 0
