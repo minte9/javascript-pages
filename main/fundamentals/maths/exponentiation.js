d6ff456a3369a7fa2588606549874fc731fc18de
minte9 <catalinprescure@gmail.com>
2022-11-22 12:56:33 +0200

    javascript-pages update

--- main/fundamentals/maths/exponentiation.js
+++ main/fundamentals/maths/exponentiation.js
@@ -8,8 +8,8 @@ console.log(2 ** 3);    // 8
 console.log(4 ** 1/2);  // 2
 console.log(8 ** 1/3);  // 2
 
-console.log(2 ^ 3); // 1 - XOR bitwise
-console.log(2 ^ 2); // 0
+console.log(2 ^ 3);     // 1 - XOR bitwise
+console.log(2 ^ 2);     // 0
 
-console.log(3 % 2); // 1 - reminder
-console.log(8 % 3); // 2
+console.log(3 % 2);     // 1 - reminder
+console.log(8 % 3);     // 2
