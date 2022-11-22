0904adb10f0e98c2497e732cae988f1328a4669f
minte9 <catalinprescure@gmail.com>
2022-11-22 09:27:42 +0200

    javascript-pages update

--- main/fundamentals/functions/function_expression.js
+++ main/fundamentals/functions/function_expression.js
@@ -1,22 +1,18 @@
 /**
  * The common syntax to create a function ...
- * is called 'function declaration'
+ * is called function declaration
  * 
- * In Javascript, a function is just another type of value.
- * So, we cand use 'function expression'
+ * In Javascript, a function is just another type of value
+ * So, we cand use function expression
  * 
- * Create a function an put it into a variable.
- * Putting a semicolon at the end is recommended, 
- * to be sure it ends the statement.
- * 
- * Function Expression are created when the exection reaches them.
- * Function Declaration it's created on initialization stage.
+ * Function Expression are created when the exection reaches them
+ * Function Declaration it's created on initialization stage
  */
 
-let sayHi = function() {
+let sayHi = function() { // Look Here
     console.log('Hello World');
-}; // Look Here
+};
 
-console.log(sayHi); // [Fuction: sayHi]
+console.log(typeof(sayHi)); // function
 
 sayHi(); // Hello World
