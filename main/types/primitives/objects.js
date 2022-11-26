f42a41fd228bc49796e4efa7c60d89fcede9e6cd
minte9 <catalinprescure@gmail.com>
2022-11-26 19:46:23 +0200

    javascript-pages update

--- main/types/primitives/objects.js
+++ main/types/primitives/objects.js
@@ -1,8 +1,7 @@
 /**
  * One of the best things about objects is that ...
- * we can store a function as one of its properties.
- * 
- * But that come with a cost
+ * we can store a function as one of its properties
+ * But that come with a cost ...
  * Objects are heavier than primitives
  */
 
