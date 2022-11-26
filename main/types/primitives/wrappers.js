f63e90fb77bce99707715812d3f2007e2d628307
minte9 <catalinprescure@gmail.com>
2022-11-26 19:42:56 +0200

    javascript-pages update

--- main/types/primitives/wrappers.js
+++ main/types/primitives/wrappers.js
@@ -1,14 +1,12 @@
 /**
  * Object wrappers for each primitive type:
- * String, Number, Boolean, Symbol, BigInt
- * 
+ * String, Number, Boolean ... 
  * Primitives can provide methods and still remain lightweight
  */
 
-let a = 'Hello World'; // Wrapper called
-let b = a.toUpperCase(); // Object destroyed, only primitive remains
-
-let c = 1.23456; // Wrapper calles
+let a = 'Hello World';      // Wrapper called
+let b = a.toUpperCase();    // Object a destroyed
+let c = 1.23456;            // Wrapper called
 let d = c.toFixed(2);
 
 console.log(b);  // HELLO WORLD
