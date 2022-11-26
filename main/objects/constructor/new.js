cbbead6f0e409ff8c758c50c2f92f3281aca7b2b
minte9 <catalinprescure@gmail.com>
2022-11-26 19:18:15 +0200

    javascript-pages update

--- main/objects/constructor/new.js
+++ main/objects/constructor/new.js
@@ -1,42 +1,31 @@
 /**
- * Constructor:
+ * New (constructor)
+ *  
+ * The 'new' operator allows to create many similar objects
+ * The regular {...} allows to create only one object
  * 
- * The regular {...} syntax allows to create one object.
- * The 'new' operator allows us to create many similar objects.
+ * Any function can be run with new
+ * The capital first letter is a convention
  * 
- * Any function can be run with new, ...
- * and it will execute the shown algorithm.
- * 
- * The capital first letter is a convention ...
- * to make it clear that can be run with new.
+ * Private constructor (Singleton):
+ * Encapsulate code that construct the object, 
+ * without future reuse
  */
 
 function User(name) {
-
-    // this = {};  // implicit
-
     this.name = name;
-
+    // this = {};   // implicit
     // return this; // implicit
 }
 
 let A = new User("Jack");
 let B = new User("Mike");
 
-console.log(A.name); // Jack
-console.log(B.name); // Mike
-
-
-/**
- * Private constructor (Singleton):
- * 
- * This encapsulate code that construct the object, ...
- * without future reuse.
- */
-
- let user = new function(name) {
+let user = new function(name) {
     this.name = "John";
     this.isAdmin = true;
 }
 
-console.log(user.isAdmin); // true
+console.log(A.name);        // Jack
+console.log(B.name);        // Mike
+console.log(user.isAdmin);  // true
