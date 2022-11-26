a16615902817cfedd9e77a21735e57bc6314e99d
minte9 <catalinprescure@gmail.com>
2022-11-26 19:23:34 +0200

    javascript-pages update

--- main/objects/constructor/methods.js
+++ main/objects/constructor/methods.js
@@ -1,9 +1,9 @@
 /**
- * We can add methods to constructor, but ...
- * for more complex syntax, we will use classes.
+ * We can add methods to constructor
+ * For more complex syntax, we will use classes
  * 
  * JavaScript provides constructor functions for many 
- * built-in language objects: like Date or Set.
+ * built-in language objects: like Date or Set
  */
 
 function User(name, birthdate) {
@@ -11,12 +11,10 @@ function User(name, birthdate) {
     this.name = name;
     this.age = new Date().getFullYear() - new Date(birthdate).getFullYear();
     this.isAdult = this.age >= 18;
-    
     this.show = () => {
         console.log(this.name + ' is adult: ' + this.isAdult);
     }
 }
 
 let user = new User("John", '01-01-1980');
-
 user.show(); // John is adult: true
