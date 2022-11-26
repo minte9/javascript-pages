a0e8512660c8c1432297818a2892c1bf8d22f460
minte9 <catalinprescure@gmail.com>
2022-11-26 20:28:56 +0200

    javascript-pages update

--- main/types/arrays/methods.js
+++ main/types/arrays/methods.js
@@ -1,5 +1,7 @@
 /**
- * Array methods: filter, map, sort
+ * Array methods
+ * 
+ * filter() map() sort()
  */
 
 let A = [
@@ -8,19 +10,15 @@ let A = [
     {id: 3, user: 'Marry'},
 ];
 let B = A.filter(item => item.id < 2);
-console.log(B); 
-    // {id: 1, user: 'John'}
-
 let C = ["Bilbo", "Gandalf"];
 let D = C.map(item => item.length);
-console.log(D); 
-    // [5, 7]
-
 let E = [
     {id: 12, user: 'John'},
     {id: 2, user: 'Pete'},
     {id: 3, user: 'Marry'},
 ];
 E.sort((a, b) => a.id - b.id);
-console.log(E[0]); 
-    // {id: 2, user: 'Pete'},
+
+console.log(B);     // {id: 1, user: 'John'}
+console.log(D);     // [5, 7]
+console.log(E[0]);  // {id: 2, user: 'Pete'},
