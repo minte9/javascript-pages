8feb289c3cf459d818c055063fe72a8af1143109
minte9 <catalinprescure@gmail.com>
2022-11-26 20:01:56 +0200

    javascript-pages update

--- main/types/numbers/round.js
+++ main/types/numbers/round.js
@@ -5,7 +5,7 @@
 let n = 1.23456;
 
 let a = Math.round(n * 100) / 100; // 123 -> 1.23
-console.log(a); // 1.23
-
 let b = n.toFixed(2); // "1.23"
+
+console.log(a); // 1.23
 console.log(1.23 === b); // false
