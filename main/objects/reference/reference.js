0b6af34cc0404e555001bec2b9d5bf3bd37ae22f
minte9 <catalinprescure@gmail.com>
2022-11-26 15:33:16 +0200

    javascript-pages update

--- main/objects/reference/reference.js
+++ main/objects/reference/reference.js
@@ -1,22 +1,22 @@
 /**
- * In Javascript a variable is alwayas copied as value.
+ * Variable, alwayas copied as value
+ * Objects, stored by reference
  * 
- * Objects are different, they are stored by reference.
- * 
- * When we copy an object, the resulted two variables are ...
- * references to tht same object (that address in computer memory).
+ * When we copy an object, the resulted two variables ...
+ * are references to tht same object
  */
 
 let a = "Hello World";
-let b = a;
-
-console.log(a === b); // true
-
-let c = {
+let A = {
     'name': 'John',
     'age': 30,
 }
-let d = c;
-d.name = 'Sam';
 
-console.log(c.name); // Sam (not John) - Look Here
+let x = a; // copy by value
+let B = A; // copy by reference
+
+B.name = 'Sam'; // changes reference object
+
+console.log(A === B);           // true
+console.log(A.name == 'Sam');   // true (copy by reference)
+console.log(A.name != 'John');  // true
