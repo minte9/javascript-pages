f70385e4df40cace13fa9e215f34444f939b91b0
minte9 <catalinprescure@gmail.com>
2022-12-01 13:48:22 +0200

    javascript-pages update

--- main/objects/basics/create.js
+++ main/objects/basics/create.js
@@ -8,15 +8,15 @@ let B = {
     name: "John",
     age: 30,  // trailing comma accepted
 };
-let myvar = "my key";
+let x = "my key";
 let C = {
     "some key": 1,
-    [myvar]: 3, // computed properties
+    [x]: 3, // computed properties
 }
 
-console.assert(
-    A.something == 'undefined' + 
-    B.name == 'John' +
-    B.age == 30 +
-    C[myvar] == 3
-);
+console.assert(A.something == undefined, true);
+console.assert(B.name == 'John', true);
+console.assert(B.age == 30, true);
+console.assert(C[x] == 3);
+
+console.log("Tests passed");
